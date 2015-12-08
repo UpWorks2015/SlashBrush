f3115de82c5fadf8e80e760957c7632de5f0a404
hytkgami <gagagatrump@gmail.com>
2015-12-08 17:16:06 +0900

    add all

--- Slash x Blash/Assets/MyFile/Scripts/StageCameraSwitch.cs
+++ Slash x Blash/Assets/MyFile/Scripts/StageCameraSwitch.cs
@@ -6,14 +6,14 @@ public class StageCameraSwitch : MonoBehaviour {
 	public Camera main_camera;
 	public Camera sub_camera;
 	public Camera subsub_camera;
-	public Camera clear_camera;
+	// public Camera clear_camera;
 
 	// Use this for initialization
 	void Start () {
-		main_camera.enabled = false;
+		main_camera.enabled = true;
 		sub_camera.enabled = false;
 		subsub_camera.enabled = false;
-		clear_camera.enabled = false;
+		// clear_camera.enabled = false;
 	}
 
 	// Update is called once per frame
@@ -23,26 +23,26 @@ public class StageCameraSwitch : MonoBehaviour {
 				main_camera.enabled = true;
 				sub_camera.enabled = false;
 				subsub_camera.enabled = false;
-				clear_camera.enabled = false;
-			} 
+				// clear_camera.enabled = false;
+			}
 			else if (DamageScript.stageId == 1) {
 				main_camera.enabled = false;
 				sub_camera.enabled = true;
 				subsub_camera.enabled = false;
-				clear_camera.enabled = false;
+				// clear_camera.enabled = false;
 			}
 			else if (DamageScript.stageId == 2){
 				main_camera.enabled = false;
 				sub_camera.enabled = false;
 				subsub_camera.enabled = true;
-				clear_camera.enabled = false;
+				// clear_camera.enabled = false;
 			}
 			else if (DamageScript.stageId == 3){
 				main_camera.enabled = false;
 				sub_camera.enabled = false;
 				subsub_camera.enabled = false;
-				clear_camera.enabled = true;
+				// clear_camera.enabled = true;
 			}
-		}	
+		}
 	}
 }
--- Slash x Blash/Assets/Scripts/DamageScript.cs
+++ Slash x Blash/Assets/Scripts/DamageScript.cs
@@ -3,92 +3,75 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class DamageScript: MonoBehaviour {
-  private Animator anim;
-  public static int stageId = 0;
-  public static bool locked = false; // ロック処理に使うフラグ
-  Slider _slider;
-  public static float _point;
-  public static bool DeathFlg = false;
+    private Animator anim;
+    public static int stageId = 0;
+    //public static int  targetEnemy = 0;
+    // public GameObject hitEffect;
+    // public GameObject hitEffect2;
+    // public GameObject hitEffect3;
+    // public AudioClip  hitsound;
+    // public AudioClip  hitsound2;
+    // public AudioClip  hitsound3;
+    // public AudioSource speaker;
+    // public AudioSource speaker2;
+    // public AudioSource speaker3;
 
-  // 自分ステータス
-  float myHp;
-  // ATK, DEF, SP
-  float[] myStatus = { 100f, 100f, 100f };
+    // GameObject AtkParticle;
+    // GameObject AtkParticle2;
+    // GameObject Atkparticle3;
+    // float randomPosX;
+    // float randomPosY;
 
-  // 敵ステータス
-  string[] enemy = new string[] { "InuSlider", "SaruSlider", "KijiSlider" };
-  float[] enemyHp = new float[]{ 5000f, 8000f, 12500f};
-  float[] enemyAtk = new float[]{ 120f, 200f, 200000f};
-  float[] enemyDef = new float[]{ 50f, 80f, 500000f};
-  float damage = 0;
+    Slider _slider;
+    string[] enemy = new string[] { "InuSlider", "SaruSlider", "KijiSlider" };
 
-  void Start () {
-    anim = GetComponent<Animator>();
-    _slider = GameObject.Find(enemy[stageId]).GetComponent<Slider>();
-    myHp = 1000f;
-  }
-
-  //float _hp = 100;
-  void Update () {
-  //敵を倒した後の
-    _slider = GameObject.Find(enemy[stageId]).GetComponent<Slider>();
-    _slider.maxValue = enemyHp[stageId];
-    Debug.Log("myHP: "+myHp);
-    Debug.Log("EnemyHP: "+enemyHp[stageId]);
-
-    // 自分が生きている間の処理
-    if(myHp > 0) {
-      if(SendEventScript.actFlg){
-        Debug.Log(SendEventScript.act);
-        if(SendEventScript.act == "Attack") {
-          damage = Damage(myStatus[0], enemyDef[stageId]);
-          enemyHp[stageId] -= damage;
-          _slider.value = enemyHp[stageId];
-          // 敵が生きている間の処理
-          if(enemyHp[stageId] > 0) {
-            // デバッグ用に自分を無敵にする
-            /*
-            if(Random.value >= 0.5) {
-              damage = Damage(enemyAtk[stageId], myStatus[1]);
-              myHp -= damage;
-            }
-            */
-          } else {
-            anim.SetBool("die",true);
-            Invoke("StageChange", 4.5f);
-          }
-      }
-      SendEventScript.act = null;
-      SendEventScript.actFlg = false;
+    void Start () {
+        anim = GetComponent<Animator>();
+        _slider = GameObject.Find(enemy[stageId]).GetComponent<Slider>();
     }
-  }else{
-    Debug.Log("GameOver");
-  }
-}
 
-/*
- * params 攻撃力, 相手防御力
- */
-float Damage(float atk, float def) {
-  return atk * atk / def - def;
-}
+    float _hp = 100;
+    void Update () {
+        _slider.value = _hp;
+        // randomPosX = Random.Range(-1.5f,1.5f);
+        // randomPosY = Random.Range(-1.5f,1.5f);
+    }
 
-void StageChange()
-{
-  if (!locked) {
-    stageId++;
-    if (stageId > 2) stageId = 2;
-  }
-  Lock();
-}
- 
-// stageIdが増加しつづけるのを防ぐ
-void Lock()
-{
-  if(locked) {
-    locked = false;
-    return;
-  }
-  locked = true;
-  }
+  //public void OnMouseDown (){
+		//_hp -= 20f;
+		//AtkParticle = Instantiate (hitEffect, new Vector3( transform.position.x+randomPosX,transform.position.y+randomPosY, transform.position.z+1),Quaternion.identity) as GameObject;
+		//Invoke ("ParticleDestroy", 0.3f);
+		//speaker.PlayOneShot(hitsound);
+		// エフェクト発生
+		//AtkParticle2 = Instantiate (hitEffect2, new Vector3( transform.position.x+randomPosX,transform.position.y+randomPosY, transform.position.z+1),Quaternion.identity) as GameObject;
+		//Invoke ("ParticleDestroy", 0.1f);
+		//speaker2.PlayOneShot(hitsound2);
+		// エフェクト発生 2
+	//}
+	public void OnMouseDown (){
+        // Atkparticle3 = Instantiate (hitEffect3, new Vector3( transform.position.x,transform.position.y,transform.position.z+2),Quaternion.identity) as GameObject;
+        // Invoke ("ParticleDestroy", 0.01f);
+        // speaker3.PlayOneShot(hitsound3);
+        //エフェクト発生 3
+        _hp -= 20;
+        _slider.value = _hp;
+        Debug.Log("hit");
+        if (_hp <= 0) {
+            anim.SetBool ("die", true);
+            if (stageId != enemy.Length) {
+                Invoke ("StageChage", 4.5f);
+            }
+        }
+    }
+
+    void StageChage()
+    {
+        stageId++;
+        Debug.Log("StageChage");
+    }
+
+  //   void ParticleDestroy()
+	// {
+        // Destroy (AtkParticle);
+  //   }
 }

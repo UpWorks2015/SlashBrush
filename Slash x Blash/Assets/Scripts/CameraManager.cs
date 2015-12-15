6c650dec04233576fb7e8193abf0ab9b271ecf68
yuta.y <mkapp.4smile.23@gmail.com>
2015-12-15 21:10:42 +0900

    fix Scripts

--- Slash x Blash/Assets/Scripts/CameraManager.cs
+++ Slash x Blash/Assets/Scripts/CameraManager.cs
@@ -21,9 +21,10 @@ public class CameraManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		TitleCam.enabled = false;
+		//Title
+		TitleCam.enabled = true;
 		StageSelectCam.enabled = false;
-		Inu1Cam.enabled = true;
+		Inu1Cam.enabled = false;
 		Momo2Cam.enabled = false;
 		Baba3Cam.enabled = false;
 		Buta1Cam.enabled = false;
@@ -38,7 +39,44 @@ public class CameraManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update (){
+		if (Input.GetKeyDown ("space")) {
+			Debug.Log ("Change");
+			CameraId++;
+		}
 		if (CameraId == 1) {
+			//StageSelect
+			TitleCam.enabled = false;
+			StageSelectCam.enabled = true;
+			Inu1Cam.enabled = false;
+			Momo2Cam.enabled = false;
+			Baba3Cam.enabled = false;
+			Buta1Cam.enabled = false;
+			Goku2Cam.enabled = false;
+			Sanzo3Cam.enabled = false;
+			Kobito1Cam.enabled = false;
+			Mama2Cam.enabled = false;
+			Ouji3Cam.enabled = false;
+			ClearCam.enabled = false;
+			GameOverCam.enabled = false;
+
+		} else if (CameraId == 2) {
+			//Inu
+			TitleCam.enabled = false;
+			StageSelectCam.enabled = false;
+			Inu1Cam.enabled = true;
+			Momo2Cam.enabled = false;
+			Baba3Cam.enabled = false;
+			Buta1Cam.enabled = false;
+			Goku2Cam.enabled = false;
+			Sanzo3Cam.enabled = false;
+			Kobito1Cam.enabled = false;
+			Mama2Cam.enabled = false;
+			Ouji3Cam.enabled = false;
+			ClearCam.enabled = false;
+			GameOverCam.enabled = false;
+
+		} else if (CameraId ==  3) {
+			//Momo
 			TitleCam.enabled = false;
 			StageSelectCam.enabled = false;
 			Inu1Cam.enabled = false;
@@ -53,7 +91,8 @@ public class CameraManager : MonoBehaviour {
 			ClearCam.enabled = false;
 			GameOverCam.enabled = false;
 
-		} else if (CameraId == 2) {
+		} else if (CameraId == 4) {
+			//Baba
 			TitleCam.enabled = false;
 			StageSelectCam.enabled = false;
 			Inu1Cam.enabled = false;
@@ -67,8 +106,105 @@ public class CameraManager : MonoBehaviour {
 			Ouji3Cam.enabled = false;
 			ClearCam.enabled = false;
 			GameOverCam.enabled = false;
-			
+
+		} else if (CameraId ==  5) {
+			//Buta
+			TitleCam.enabled = false;
+			StageSelectCam.enabled = false;
+			Inu1Cam.enabled = false;
+			Momo2Cam.enabled = false;
+			Baba3Cam.enabled = false;
+			Buta1Cam.enabled = true;
+			Goku2Cam.enabled = false;
+			Sanzo3Cam.enabled = false;
+			Kobito1Cam.enabled = false;
+			Mama2Cam.enabled = false;
+			Ouji3Cam.enabled = false;
+			ClearCam.enabled = false;
+			GameOverCam.enabled = false;
+
+		} else if (CameraId == 6) {
+			//Goku
+			TitleCam.enabled = false;
+			StageSelectCam.enabled = false;
+			Inu1Cam.enabled = false;
+			Momo2Cam.enabled = false;
+			Baba3Cam.enabled = false;
+			Buta1Cam.enabled = false;
+			Goku2Cam.enabled = true;
+			Sanzo3Cam.enabled = false;
+			Kobito1Cam.enabled = false;
+			Mama2Cam.enabled = false;
+			Ouji3Cam.enabled = false;
+			ClearCam.enabled = false;
+			GameOverCam.enabled = false;
+
+		} else if (CameraId == 7) {
+			//Sanzo
+			TitleCam.enabled = false;
+			StageSelectCam.enabled = false;
+			Inu1Cam.enabled = false;
+			Momo2Cam.enabled = false;
+			Baba3Cam.enabled = false;
+			Buta1Cam.enabled = false;
+			Goku2Cam.enabled = false;
+			Sanzo3Cam.enabled = true;
+			Kobito1Cam.enabled = false;
+			Mama2Cam.enabled = false;
+			Ouji3Cam.enabled = false;
+			ClearCam.enabled = false;
+			GameOverCam.enabled = false;
+
+		} else if (CameraId == 8) {
+			//Kobito
+			TitleCam.enabled = false;
+			StageSelectCam.enabled = false;
+			Inu1Cam.enabled = false;
+			Momo2Cam.enabled = false;
+			Baba3Cam.enabled = false;
+			Buta1Cam.enabled = false;
+			Goku2Cam.enabled = false;
+			Sanzo3Cam.enabled = false;
+			Kobito1Cam.enabled = true;
+			Mama2Cam.enabled = false;
+			Ouji3Cam.enabled = false;
+			ClearCam.enabled = false;
+			GameOverCam.enabled = false;
+
+		} else if (CameraId == 9) {
+			//Mama
+			TitleCam.enabled = false;
+			StageSelectCam.enabled = false;
+			Inu1Cam.enabled = false;
+			Momo2Cam.enabled = false;
+			Baba3Cam.enabled = false;
+			Buta1Cam.enabled = false;
+			Goku2Cam.enabled = false;
+			Sanzo3Cam.enabled = false;
+			Kobito1Cam.enabled = false;
+			Mama2Cam.enabled = true;
+			Ouji3Cam.enabled = false;
+			ClearCam.enabled = false;
+			GameOverCam.enabled = false;
+
+		} else if (CameraId == 10) {
+			//Ouji
+			TitleCam.enabled = false;
+			StageSelectCam.enabled = false;
+			Inu1Cam.enabled = false;
+			Momo2Cam.enabled = false;
+			Baba3Cam.enabled = false;
+			Buta1Cam.enabled = false;
+			Goku2Cam.enabled = false;
+			Sanzo3Cam.enabled = false;
+			Kobito1Cam.enabled = false;
+			Mama2Cam.enabled = false;
+			Ouji3Cam.enabled = true;
+			ClearCam.enabled = false;
+			GameOverCam.enabled = false;
+
 		} else if (CameraId == 11) {
+			//Clear
 			TitleCam.enabled = false;
 			StageSelectCam.enabled = false;
 			Inu1Cam.enabled = false;
@@ -82,6 +218,23 @@ public class CameraManager : MonoBehaviour {
 			Ouji3Cam.enabled = false;
 			ClearCam.enabled = true;
 			GameOverCam.enabled = false;
+
+		}else if (CameraId == 12) {
+			//GameOver
+			TitleCam.enabled = false;
+			StageSelectCam.enabled = false;
+			Inu1Cam.enabled = false;
+			Momo2Cam.enabled = false;
+			Baba3Cam.enabled = false;
+			Buta1Cam.enabled = false;
+			Goku2Cam.enabled = false;
+			Sanzo3Cam.enabled = false;
+			Kobito1Cam.enabled = false;
+			Mama2Cam.enabled = false;
+			Ouji3Cam.enabled = false;
+			ClearCam.enabled = false;
+			GameOverCam.enabled = true;
 		}
+
 	}
 }

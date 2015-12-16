28c79b647832f80b201151c424015b159da8ed20
yuta.y <mkapp.4smile.23@gmail.com>
2015-12-16 17:53:30 +0900

    add GameUI source

---
+++ Slash x Blash/Assets/Scripts/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+	public Canvas GameUI;
+	public Image hpBar;
+	public Image actionPoint;
+	public Image enemyAtk;
+
+	// Use this for initialization
+	void Start () {
+
+		 GameUI.enabled = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (CameraManager.cameraId >= 2 && CameraManager.cameraId < 11) {
+			GameUI.enabled = true;
+		} else {
+			GameUI.enabled = false;
+		}
+	}
+}

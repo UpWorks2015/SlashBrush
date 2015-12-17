2bc86f0fb5e7978cbf879d9c07cc2ce06e4c9ff5
yuta.y <mkapp.4smile.23@gmail.com>
2015-12-17 14:17:15 +0900

    add SendAction

--- SxB_Controller/Assets/SBController/Scripts/Controller/SendEventScript.cs
+++ SxB_Controller/Assets/SBController/Scripts/Controller/SendEventScript.cs
@@ -7,12 +7,15 @@ public class SendEventScript : MonoBehaviour {
 	public static List<string> eventList = new List<string>();
 	// Chat用のテキスト
 	private string currentMessage = string.Empty;
-
+	public static string act;
+	public static bool actFlg = false;
+	private bool isAvailable = true;
+	
 	// Use this for initialization
 	void Start () {
-
+		
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
 		// 接続が確立されていた場合の処理
@@ -20,52 +23,79 @@ public class SendEventScript : MonoBehaviour {
 		{
 			if ( ButtonScript.AtkFlg == true )
 			{
-					// 入力情報が空ではない場合処理します。
-							currentMessage = "atk";
-							string msg = Network.player.guid + ":" + currentMessage;
-
-
-
-							GetComponent<NetworkView>().RPC("chatMessage", RPCMode.All, new object[] { msg } );
-							currentMessage = string.Empty;
-							ButtonScript.AtkFlg = false;
-
+				// 入力情報が空ではない場合処理します。
+				currentMessage = "atk";
+				string msg = Network.player.guid + ":" + currentMessage;
+				
+				
+				
+				GetComponent<NetworkView>().RPC("chatMessage", RPCMode.All, new object[] { msg } );
+				currentMessage = string.Empty;
+				ButtonScript.AtkFlg = false;
+				
 			}
 			if ( ButtonScript.DefFlg == true )
 			{
-					// 入力情報が空ではない場合処理します。
-							currentMessage = "def";
-							string msg = Network.player.guid + ":" + currentMessage;
-
-
-
-							GetComponent<NetworkView>().RPC("chatMessage", RPCMode.All, new object[] { msg } );
-							currentMessage = string.Empty;
-							ButtonScript.DefFlg = false;
-
+				// 入力情報が空ではない場合処理します。
+				currentMessage = "def";
+				string msg = Network.player.guid + ":" + currentMessage;
+				
+				
+				
+				GetComponent<NetworkView>().RPC("chatMessage", RPCMode.All, new object[] { msg } );
+				currentMessage = string.Empty;
+				ButtonScript.DefFlg = false;
+				
 			}
 			if ( ButtonScript.SpFlg == true )
 			{
-					// 入力情報が空ではない場合処理します。
-							currentMessage = "sp";
-							string msg = Network.player.guid + ":" + currentMessage;
-
-
-
-							GetComponent<NetworkView>().RPC("chatMessage", RPCMode.All, new object[] { msg } );
-							currentMessage = string.Empty;
-							ButtonScript.SpFlg = false;
-
+				// 入力情報が空ではない場合処理します。
+				currentMessage = "sp";
+				string msg = Network.player.guid + ":" + currentMessage;
+				
+				
+				
+				GetComponent<NetworkView>().RPC("chatMessage", RPCMode.All, new object[] { msg } );
+				currentMessage = string.Empty;
+				ButtonScript.SpFlg = false;
+				
 			}
-
+			
 		}
 	}
 	[RPC]
 	public void chatMessage(string msg)
 	{
-			// 引数のメッセージをローカルの配列にセットする。
-			eventList.Add(msg);
-			Debug.Log(msg.Split(':')[1]);
-			//EventScript.Action(msg.Split(':')[0],msg.Split(':')[1]);
+		// 引数のメッセージをローカルの配列にセットする。
+		eventList.Add(msg);
+		Debug.Log(msg.Split(':')[1]);
+		SendAction(msg.Split(':')[0],msg.Split(':')[1]);
+	}
+	
+	public void SendAction(string player,string actType){
+		if(isAvailable) {
+			if(actType=="atk"){
+				Debug.Log(player + ":Attack");
+				act = "Attack";
+				actFlg = true;
+			}
+			else if(actType=="def"){
+				Debug.Log(player + ":Defence");
+				act = "Defence";
+				actFlg = true;
+			}
+			else if(actType=="sp"){
+				Debug.Log(player+":Magic");
+				act = "Magic";
+				actFlg = true;
+			}
+			StartCoroutine(ToggleAvailable());
+		}
+	}
+	
+	IEnumerator ToggleAvailable() {
+		isAvailable = false;
+		yield return new WaitForSeconds(3.0f);
+		isAvailable = true;
 	}
 }
